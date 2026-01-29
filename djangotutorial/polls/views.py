f0e779e54a1e846323eb4a2c8af5aacb426aa047
tapan71 <moditapan0@gmail.com>
2026-01-29 21:56:50 +0530

    Add redirect to polls in URL configuration and create detail template

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Question
 from .models import Choice
+from django.http import Http404
 
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
@@ -23,7 +25,6 @@ def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
-
+    return render(request, "polls/index.html", context)
 
 # Create your views here.
